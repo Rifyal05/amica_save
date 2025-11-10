6afb5d9887927a4a72c5e6f7ff9db53d8fdfa949
Rifyal05 <rifyalaidil05@gmail.com>
2025-11-11 05:28:45 +0700

    requirements.txt

--- creat_db.sql
+++ creat_db.sql
@@ -3,8 +3,8 @@ FROM pg_stat_activity
 WHERE pg_stat_activity.datname = 'amica_db' AND pid <> pg_backend_pid();
 
 DROP DATABASE IF EXISTS amica_db;
-DROP ROLE IF EXISTS amica_user;
+DROP ROLE IF EXISTS lymoon;
 
-CREATE ROLE amica_user WITH LOGIN PASSWORD 'YOUR_SECURE_PASSWORD';
+CREATE ROLE lymoon WITH LOGIN PASSWORD 'Moon.1234';
 
-CREATE DATABASE amica_db OWNER amica_user;
+CREATE DATABASE amica_db OWNER lymoon;
